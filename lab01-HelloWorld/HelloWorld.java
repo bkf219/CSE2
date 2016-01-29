a53e3f674d3a28f6b2b2aa8e0ac56299a1d16d94
bkf219 <bkf219@lehigh.edu>
2016-01-29 17:05:35 +0000

    lab1

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////
+// Brian Chen
+// Lab 1
+// Hello World Java Program
+//
+//  first compile the program
+//     javac HelloWorld.java
+//  run the program
+//     java HelloWorld.java
+
+//  define a class
+public class HelloWorld
+{
+
+//  add a main method
+    public static void main(String[] args) 
+    {
+// print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}

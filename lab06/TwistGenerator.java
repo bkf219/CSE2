fd5068177507b21cb1151774808547c77b6a3bc3
bkf219 <bkf219@lehigh.edu>
2016-03-04 15:39:37 +0000

    finished lab 6

---
+++ lab06/TwistGenerator.java
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////
+// Brian Falkenstein
+// CSE 2
+// Lab 3
+// Feb 12 2016
+import java.util.Scanner; 
+//this program will generate a graphical "twist" or knot of length n
+public class TwistGenerator
+{
+    public static void main(String args[])
+    {
+        Scanner kbReader = new Scanner(System.in); //create scanner
+        int n=0; //create the variable to avoid scope errors, becomes user import
+        while(true) //infinite while loop
+        {
+            System.out.println("How long would you like the twist to be? ");
+            n = kbReader.nextInt();
+            if(n>=0)
+            {
+                break; //check to see if its valid, and if so, continues with the program
+            }
+        }
+        int cycles = n/3; //amount of 3 character cycles are present
+        int leftover = n%3; //amount of characters left over
+        int i = 0, j = 0, c = 0; //different variales for while loops 
+        while(i<cycles)
+        {
+            System.out.print("\\ /"); //cycle through first line
+            i++;
+        }
+        
+        if(leftover==1)
+            System.out.print("\\\n");
+        else if(leftover==2)                   //add leftover characters
+            System.out.print("\\ \n");
+            
+        while(j<cycles)
+        {
+            System.out.print(" X ");         //cycle through second line
+            j++;
+        }
+        
+        if(leftover==1)
+            System.out.print(" \n");
+        else if(leftover==2)              //add leftovers for second line
+            System.out.print(" X\n");
+            
+        while(c<cycles)
+        {
+            System.out.print("/ \\");        //cycle through third line
+            c++;
+        }
+        
+        if(leftover==1)
+            System.out.print("/\n");        //add leftovers for third line
+        else if(leftover==2)
+            System.out.print("/ \n");
+    }
+}
